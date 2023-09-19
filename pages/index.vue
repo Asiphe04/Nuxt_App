3bfa90e20a866c2c24771fadcd5b9f80b8383189
Asiphe04 <asiphendimlana40@gmail.com>
2023-09-19 15:51:29 +0200

    NuxtLinks

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
     <h1>Home</h1>
   </div>
 </template>
---
+++ pages/products/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Products for {{ id }}</h1>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
